d689432432784893013e92240a054dcc037bd2ca
Luke Rimmo Loyi Lego <114318995+TasumLuke@users.noreply.github.com>
2023-02-25 17:06:16 -0500

    Create DOSAttack.py

---
+++ DOSAttack.py
@@ -0,0 +1,11 @@
+# DoS Attack in Python
+import socket
+import time
+import os
+
+def launch_dos_attack():
+    # TODO: add code for launching DoS attack
+    pass
+
+if __name__ == '__main__':
+    launch_dos_attack()
