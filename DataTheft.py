8e7691af3a5bed96e8532c4e28823c70000f4308
Luke Rimmo Loyi Lego <114318995+TasumLuke@users.noreply.github.com>
2023-02-25 17:05:46 -0500

    Create DataTheft.py

---
+++ DataTheft.py
@@ -0,0 +1,17 @@
+# Data Theft in Python
+import os
+
+def steal_passwords():
+    # TODO: add code for stealing passwords
+    pass
+
+def steal_credit_cards():
+    # TODO: add code for stealing credit card details
+    pass
+
+def steal_private_info():
+    # TODO: add code for stealing private information
+    pass
+
+if __name__ == '__main__':
+    steal_password
