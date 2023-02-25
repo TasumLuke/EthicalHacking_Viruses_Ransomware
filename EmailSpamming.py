13e22485c9f06159aaf3d118aacdc75c5bb5b990
Luke Rimmo Loyi Lego <114318995+TasumLuke@users.noreply.github.com>
2023-02-25 17:06:01 -0500

    Create EmailSpamming.py

---
+++ EmailSpamming.py
@@ -0,0 +1,11 @@
+# Email Spamming in Python
+import smtplib
+import email.message
+import os
+
+def spam_emails():
+    # TODO: add code for spamming emails
+    pass
+
+if __name__ == '__main__':
+    spam_emails()
